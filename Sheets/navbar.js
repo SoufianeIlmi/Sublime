7f584bf3e8da0fa8bcfd118110f4a588937b7277
Soufiane Ilmi <snowoufiane@gmail.com>
2019-02-20 21:13:11 +0100

    First Shock

---
+++ Sheets/navbar.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.barsbutton').click(function(){
+        $('#phonebar').show(1000);
+    })
+    $('.closebutton').click(function(){
+        $('#phonebar').hide(1000);
+    })
+})
